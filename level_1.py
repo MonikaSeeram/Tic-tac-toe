88a9263b94e7a9213f07d2b746974379339340c6
MonikaSeeram <110085278+MonikaSeeram@users.noreply.github.com>
2022-08-30 09:30:39 +0530

    Update level_1.py

--- level_1.py
+++ level_1.py
@@ -174,7 +174,7 @@ def with_computer(game_board):
     game_board = Tk()
     game_board.title("Tic Tac Toe")
     game_board.resizable(False, False)
-    game_board.iconbitmap("favicon.ico")
+    game_board.iconbitmap("favicon (1).ico")
     marker1 = Button(game_board, text="Player : X", width=8, font=('bold', 10), fg='black', bg="tan", state=DISABLED)
     marker1.grid(row=1, column=1)
     marker2 = Button(game_board, text="Computer : O", width=9, font=('bold', 10), fg='black', bg="tan", state=DISABLED)
@@ -188,7 +188,7 @@ def with_player(game_board):
     game_board = Tk()  # Displays the game window interface
     game_board.title("Tic Tac Toe")
     game_board.resizable(False, False)  # Disabling the resize of the gridboard
-    game_board.iconbitmap("favicon.ico")
+    game_board.iconbitmap("favicon (1).ico")
     marker1 = Button(game_board, text="Player 1 : X", width=10, state=DISABLED)
     marker1.grid(row=1, column=1)
     marker2 = Button(game_board, text="Player 2 : O", width=10, state=DISABLED)
@@ -201,9 +201,9 @@ def play():
     menu = Tk()
     menu.geometry("418x500")
     menu.title("Tic Tac Toe")
-    menu.iconbitmap("favicon.ico (1)")
+    menu.iconbitmap("favicon (1).ico")
     menu.resizable(False, False)
-    menu.img = Image.open("Temp-500x500.jpg (1)")  # Setting up the background image
+    menu.img = Image.open("Temp-500x500 (1).jpg")  # Setting up the background image
     menu.bg = ImageTk.PhotoImage(menu.img)
     welcome_frame = Label(menu, image=menu.bg)
     welcome_frame.pack(fill=BOTH, expand=True)
@@ -235,7 +235,7 @@ def play():
     toggle_button.pack(pady=30,side='bottom')
 
     def play_sound():  # Defining the function to play music.
-        pygame.mixer.music.load("Happy-Whistling-Ukulele.mp3 (1)")  # Loading the music
+        pygame.mixer.music.load("Happy-Whistling-Ukulele (1).mp3")  # Loading the music
         pygame.mixer.music.play()
 
     menu.mainloop()  # execute when we want to run our application
